c08fc8df586099198548c970065790d62bebaf82
Konstantin Timoshenko <tkoc@inbox.ru>
2022-12-11 17:57:37 +0300

    initian commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 6_843;
+        int bonus= price/20;
+        System.out.println("Бонус"+bonus);
+
+
+
+
+
+
+    }
+}
